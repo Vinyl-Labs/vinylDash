e5653ebb12df918173d17fe89137fb4923925183
SlayTheSensei <kylejpearson@gmail.com>
2018-11-20 21:46:50 -0500

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import * as Firebase from "firebase";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyA_TENXrDQhNbZViTMa1nmLAovqvJ-4jH8",
+  authDomain: "vinyl-56cb9.firebaseapp.com",
+  databaseURL: "https://vinyl-56cb9.firebaseio.com",
+  messagingSenderId: "250378865774",
+  projectId: "vinyl-56cb9",
+  storageBucket: "vinyl-56cb9.appspot.com"
+};
+
+Firebase.initializeApp(config);
+Firebase.firestore().settings({ timestampsInSnapshots: true });
+
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+registerServiceWorker();
