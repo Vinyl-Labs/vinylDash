5b9df4192a4e9da33a09e16b73dc465bb4e1a18a
SlayTheSensei <kylejpearson@gmail.com>
2019-08-16 15:02:16 -0400

    Added initial Route

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
+import { Route } from "react-router";
 import { HomeContainer } from "src/styles/styled_components/home";
 import { Artists } from "./artists";
 
 function Home() {
   return (
     <HomeContainer>
-      {/* TODO: Add Routes */}
-      <Artists />
+      <Route exact={true} path="/" component={Artists} />
     </HomeContainer>
   );
 }
